3bf6e7b50e396fbc0a4d496309ddbf03e548ad47
Rob Wagner <rob@sombia.com>
2023-11-22 20:48:37 -0500

    Remove commented out code

--- bufferfish-ts/src/bufferfish.ts
+++ bufferfish-ts/src/bufferfish.ts
@@ -409,12 +409,4 @@ export class Bufferfish {
 
         return str
     }
-
-    // public serialize = (obj: object) => {}
-
-    // public serializeNumber = (number: number) => {}
-
-    // public serializeString = (string: string) => {}
-
-    // public serializeBoolean = (bool: boolean) => {}
 }
