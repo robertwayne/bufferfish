5d9318d980721c8158654ffa23f06a372620349f
Rob Wagner <rob@sombia.com>
2024-01-24 00:05:17 -0500

    Better error message when unwrapping field names

--- bufferfish-rs/bufferfish-derive/src/lib.rs
+++ bufferfish-rs/bufferfish-derive/src/lib.rs
@@ -17,11 +17,9 @@ pub fn bufferfish_serializer(input: TokenStream) -> TokenStream {
         Data::Struct(data) => match &data.fields {
             Fields::Named(fields) => {
                 for field in &fields.named {
-                    let ident = field.ident.as_ref();
-
-                    if ident.is_none() {
+                    let Some(ident) = field.ident.as_ref() else {
                         abort!(field.span(), "only named fields are supported");
-                    }
+                    };
 
                     let ty = &field.ty;
 
