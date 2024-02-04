6c5183127251b0dbe74d16ef88af4ff1808d735a
Rob Wagner <rob@sombia.com>
2024-02-03 19:27:56 -0500

    Add unit and tuple struct tests

--- bufferfish-rs/bufferfish-derive/tests/serialize_struct.rs
+++ bufferfish-rs/bufferfish-derive/tests/serialize_struct.rs
@@ -12,3 +12,25 @@ fn serialize_struct() {
 
     assert_eq!(bf.len(), 1);
 }
+
+#[test]
+fn serialize_unit_struct() {
+    #[derive(Serialize, Debug)]
+    struct Foo;
+
+    let foo = Foo;
+    let bf: Bufferfish = foo.to_bufferfish().unwrap();
+
+    assert_eq!(bf.len(), 0);
+}
+
+#[test]
+fn serialize_tuple_struct() {
+    #[derive(Serialize, Debug)]
+    struct Foo(u8);
+
+    let foo = Foo(42);
+    let bf: Bufferfish = foo.to_bufferfish().unwrap();
+
+    assert_eq!(bf.len(), 1);
+}
