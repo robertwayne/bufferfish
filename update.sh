491b8b7c626fe54dd6d863fd9957b715c961634d
Rob Wagner <rob@sombia.com>
2024-02-14 20:21:04 -0500

    Remove old update script

--- update.sh
+++
@@ -1,9 +0,0 @@
-echo "Updating TypeScript dependencies..."
-cd bufferfish-ts
-pnpm upgrade &> /dev/null
-
-echo "Updating Rust dependencies..."
-cd ../bufferfish-rs
-cargo update --quiet
-
-echo "Bufferfish is all up to date! ✨"
