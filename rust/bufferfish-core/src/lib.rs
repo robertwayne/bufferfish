1cdcda4d9290e79dae52aafb254a5d45e73d65fb
Rob Wagner <rob@sombia.com>
2024-06-16 14:49:48 -0400

    Fix ci error

--- rust/bufferfish-core/src/lib.rs
+++ rust/bufferfish-core/src/lib.rs
@@ -120,7 +120,7 @@ impl Bufferfish {
         self.inner.get_ref().to_vec()
     }
 
-    /// Returns an `Arc<[u8]>` of the internal byte buffer for cheaply cloning 
+    /// Returns an `Arc<[u8]>` of the internal byte buffer for cheaply cloning
     /// and sharing the buffer.
     pub fn as_bytes(&self) -> Arc<[u8]> {
         self.inner.get_ref().clone().into()
