72f905134a00ad5ab529152036cc5214f177a258
Rob Wagner <rob@sombia.com>
2025-05-16 22:09:59 -0400

    Add bun tests to ci; add script for running ci commands locally

---
+++ ci.sh
@@ -0,0 +1,9 @@
+cd rust
+cargo clippy --workspace --all-targets --all-features -- -D warnings
+cargo fmt --all -- --check
+cargo test -p bufferfish-core -p bufferfish -p bufferfish-derive --lib --all-features --all-targets
+
+cd ../typescript
+bun tsc
+bun fmt:check
+bun test
